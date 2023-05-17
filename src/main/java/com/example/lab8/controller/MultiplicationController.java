129038e4efdff7fb813205a766f68d2e7af1d64b
Vladlen <ilusenkovladlen@gmail.com>
2023-05-17 16:10:27 +0300

    Added more boilerplate code

--- src/main/java/com/example/lab8/controller/MultiplicationController.java
+++ src/main/java/com/example/lab8/controller/MultiplicationController.java
@@ -1,13 +1,19 @@
 package com.example.lab8.controller;
 
+import com.example.lab8.service.MultiplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class MultiplicationController {
-    @GetMapping("/multiply-client")
+    @Autowired
+    private MultiplicationService multiplicationService;
+
+    @PostMapping("/multiply-client")
     public String clientMultiplication() {
         return "ok";
     }
---
+++ src/main/java/com/example/lab8/service/MultiplicationService.java
@@ -0,0 +1,22 @@
+package com.example.lab8.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MultiplicationService {
+    public void multiplyMatricesFromClient() {
+
+    }
+
+    public void multiplyMatricesFromServer() {
+
+    }
+
+    private void readMatrixFromFile() {
+
+    }
+
+    private void multiplyMatrices() {
+
+    }
+}
