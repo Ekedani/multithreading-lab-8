155c513a21883062fdd9a75741a672804f798c23
Vladlen <ilusenkovladlen@gmail.com>
2023-05-18 17:35:55 +0300

    Added reading matrices from file

--- src/main/java/com/example/lab8/service/MultiplicationService.java
+++ src/main/java/com/example/lab8/service/MultiplicationService.java
@@ -5,10 +5,14 @@ import com.example.lab8.model.MultiplicationRequest;
 import com.example.lab8.service.algorithm.FoxMatrixMultiplicator;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 @Service
 public class MultiplicationService {
     private final static int BLOCKS_NUM_SQRT = 4;
+    private final static String DATA_DIRECTORY = "D:\\KPI\\H1'23\\Parallel\\lab-8\\data\\";
 
     public double[][] multiplyMatricesFromClient(MultiplicationRequest multiplicationRequest) {
         var a = new Matrix(multiplicationRequest.a);
@@ -26,7 +30,23 @@ public class MultiplicationService {
     }
 
     private Matrix readMatrixFromFile(int matrixSize) {
-        return new Matrix(matrixSize);
+        try {
+            var file = new File(DATA_DIRECTORY + matrixSize + ".csv");
+            var reader = new BufferedReader(new FileReader(file));
+            double[][] matrix = new double[matrixSize][matrixSize];
+            String line;
+            int row = 0;
+            while ((line = reader.readLine()) != null && row < matrixSize) {
+                String[] values = line.split(",");
+                for (int col = 0; col < matrixSize; col++) {
+                    matrix[row][col] = Integer.parseInt(values[col]);
+                }
+                row++;
+            }
+            return new Matrix(matrix);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Matrix multiplyMatrices(Matrix a, Matrix b) {
